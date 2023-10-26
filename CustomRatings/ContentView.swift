d54bd9ead5f6b3e8be5a3c214403387b1ba281fe
Stewart Lynch <slynch@createchsol.com>
2023-10-26 16:53:18 -0700

    Initial Commit

---
+++ CustomRatings/ContentView.swift
@@ -0,0 +1,28 @@
+//
+// Created for CustomRatings
+// by  Stewart Lynch on 2023-10-26
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

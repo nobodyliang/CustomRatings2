6ba5e18781e4f46a54582063d8f3376edfdae3ff
Stewart Lynch <slynch@createchsol.com>
2023-10-28 09:29:42 -0700

    FEAT: Added Start Ratings View
    Created a custom RatingsView that will allow the user to provide a max ratings,
    a bound currentRating and a width to determine the width of the view to be
    displayed and the size of the stars.
    The RatingsView will display stars for the max rating which includes a clear
    star along with a number of stars in an HStack to represent from 0..<maxRating
    Tapping on one of the starts will set the current rating.

---
+++ CustomRatings/RatingsView.swift
@@ -0,0 +1,67 @@
+//
+// Created for CustomRatings
+// by  Stewart Lynch on 2023-10-28
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+struct RatingsView: View {
+    let maxRating: Int
+    @Binding var currentRating: Int
+    let width: Int
+    
+    init(maxRating: Int, currentRating: Binding<Int>, width: Int = 30) {
+        self.maxRating = maxRating
+        self._currentRating = currentRating
+        self.width = width
+    }
+    var body: some View {
+        HStack {
+            Image(systemName: "star")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.yellow)
+                .symbolVariant(.slash)
+                .opacity(currentRating == 0 ? 0 : 1)
+                .onTapGesture {
+                    withAnimation {
+                        currentRating = 0
+                    }
+                }
+            ForEach(0..<maxRating, id: \.self) { rating in
+                Image(systemName: "star")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.yellow)
+                    .symbolVariant(rating < currentRating ? .fill : .none)
+                    .onTapGesture {
+                        withAnimation {
+                            currentRating = rating + 1
+                        }
+                    }
+            }
+        }
+        .frame(width: CGFloat(maxRating * width))
+    }
+}
+
+#Preview {
+    struct PreviewWapper: View {
+        let maxRating = 5
+        @State var currentRating = 2
+        var body: some View {
+            RatingsView(
+                maxRating: maxRating,
+                currentRating: $currentRating
+            )
+            
+        }
+    }
+    return PreviewWapper()
+}
